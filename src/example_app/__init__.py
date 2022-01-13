41473d2ddeacf7a9c8413c15ca7ecac601717fa4
root <root@api.ecoquintas.com>
2022-01-13 15:28:09 +0000

    asd

--- src/example_app/__init__.py
+++ src/example_app/__init__.py
@@ -15,32 +15,34 @@ producer = Producer(conf)
 
 #Funciones de producción de Topicos
 def producir_ventas(data):
-        producer.produce("Log_ventas", key="ventas", value=data)
+        producer.produce("Log_ventas_app", key="ventas", value=data)
         producer.flush()
 
 def producir_CRM(data):
-        producer.produce("Log_CRM", key="CRM", value=data)
+        producer.produce("Log_CRM_app", key="CRM", value=data)
         producer.flush()
 
-def producir_Contactos(data):
+def producir_dwh(data):
+        producer.produce("Log_dwh", key="dwh", value=data)
+        producer.flush() 
+
+
+##################################################################################################################################################################################
+""" def producir_Contactos(data):
         producer.produce("Log_Contactos", key="contactos", value=data)
         producer.flush()
-
 def producir_Pagos(data):
         producer.produce("Log_Pagos", key="pagos", value=data)
         producer.flush()
-
 def producir_Casos(data):
         producer.produce("Log_Casos", key="casos", value=data)
         producer.flush()
 
 def producir_medios(data):
         producer.produce("Log_medios", key="medios", value=data)
-        producer.flush()
+        producer.flush() """
+##################################################################################################################################################################################
 
-def producir_dwh(data):
-        producer.produce("Log_dwh", key="dwh", value=data)
-        producer.flush()
 #Funciones de prod_message  
 
 
@@ -50,7 +52,7 @@ def producir_dwh(data):
 def root():
     return "Hola API ECOQUINTAS"
 
-@app.route("/producer/ventas/",methods=['POST'])
+@app.route("/producer/ventas_app/",methods=['POST'])
 def prod_ventas():
     token = request.headers.get('token')
     if (token=="3e26b17c-3e96-40d6-91fa-7f355bf2c570"):
@@ -62,7 +64,7 @@ def prod_ventas():
     else:
         return "Token invalido"
 
-@app.route("/producer/CRM/",methods=['POST'])
+@app.route("/producer/CRM_app/",methods=['POST'])
 def prod_CRM():
     token = request.headers.get('token')
     if (token=="3e26b17c-3e96-40d6-91fa-7f355bf2c570"):
@@ -74,7 +76,21 @@ def prod_CRM():
     else:
         return "Token invalido"
 
-@app.route("/producer/contactos/",methods=['POST'])
+@app.route("/producer/dwh/",methods=['POST'])
+def prod_dwh():
+    token = request.headers.get('token')
+    if (token=="3e26b17c-3e96-40d6-91fa-7f355bf2c570"):
+        content = request.json
+        content["ip"]=request.remote_addr
+        respuesta={"dwh":content}
+        producir_dwh(json.dumps(respuesta))
+        return respuesta
+    else:
+        return "Token invalido"
+
+
+###########################################################################################################################################################################################
+""" @app.route("/producer/contactos/",methods=['POST'])
 def prod_ontactos():
     token = request.headers.get('token')
     if (token=="3e26b17c-3e96-40d6-91fa-7f355bf2c570"):
@@ -121,15 +137,5 @@ def prod_medios():
         return respuesta
     else:
         return "Token invalido"
-
-@app.route("/producer/dwh/",methods=['POST'])
-def prod_dwh():
-    token = request.headers.get('token')
-    if (token=="3e26b17c-3e96-40d6-91fa-7f355bf2c570"):
-        content = request.json
-        content["ip"]=request.remote_addr
-        respuesta={"dwh":content}
-        producir_dwh(json.dumps(respuesta))
-        return respuesta
-    else:
-        return "Token invalido"
+         """
+############################################################################################################################################################################################
